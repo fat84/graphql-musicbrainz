a1d7a24e9e7597ceedf40555d476cca8678c19e5
lawshe <jessica@jlawshe.com>
2017-04-09 18:49:34 -0500

    Component for album

---
+++ app/components/partials/Album.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default (props) => {
+  let coverJsx = (props.album && props.album.node && props.album.node.coverArt && props.album.node.coverArt.front) ?
+  <div className="card-image">
+    <img src={props.album.node.coverArt.front}/>
+  </div>
+  : <div></div>;
+
+
+  return (
+    <div className="col s12 m6 l3 album-col">
+      <div className="card">
+        {coverJsx}
+        <div className="card-content">
+          <span className="card-title">
+            {props.album.node.title}
+          </span>
+          {props.album.node.date}
+        </div>
+      </div>
+    </div>
+  );
+}
