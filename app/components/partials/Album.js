4865c2c6331e0671fe100bf04e527cce94032136
lawshe <jessica@jlawshe.com>
2017-04-09 22:00:08 -0500

    Using date partial alias; Added label JSX

--- app/components/partials/Album.js
+++ app/components/partials/Album.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Date from './date.js';
+import Date from 'datePartial';
 
 export default (props) => {
+  console.log('props.album.node',props.album.node);
   let coverJsx = (props.album && props.album.node && props.album.node.coverArt && props.album.node.coverArt.front) ?
   <div className="card-image">
     <img src={props.album.node.coverArt.front}/>
@@ -11,6 +12,13 @@ export default (props) => {
   let dateJsx = (props.album && props.album.node && props.album.node.date) ?
     <Date date={props.album.node.date}/>
   : <span></span>
+
+  let labelsJsx = (props.album && props.album.node && props.album.node.labels && props.album.node.labels.edges&& props.album.node.labels.edges.length>0) ?
+    props.album.node.labels.edges.map((label, i) => {
+      console.log('label', label);
+      return <div>{label.node.name}</div>
+    })
+  : <div></div>;
 
 
   return (
@@ -22,6 +30,7 @@ export default (props) => {
             {props.album.node.title}
           </span>
           <p>{dateJsx}</p>
+          {labelsJsx}
         </div>
       </div>
     </div>
