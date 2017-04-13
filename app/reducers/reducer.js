e97b258e580cbd5d57bc0ae9fbe34a93d8cd6da6
lawshe <jessica@jlawshe.com>
2017-04-13 11:21:27 -0500

    Reducers for SET_ARTIST and SET_PAGINATION_LOCATON
    Using SET_PAGINATION_LOCATON to show location within results. Not
    currently using artist_name from store.

--- app/reducers/reducer.js
+++ app/reducers/reducer.js
@@ -2,7 +2,9 @@ import Immutable from 'immutable';
 const immutableState = Immutable.Map({
   fetching: false,
   data: Immutable.Map({}),
-  show_albums: new Set()
+  show_albums: new Set(),
+  artist_name: null,
+  pagination_location: 1
 })
 
 export const queryReducer = (state = immutableState, action) => {
@@ -15,6 +17,8 @@ export const queryReducer = (state = immutableState, action) => {
         return state.set('fetching', false).set('data', Immutable.Map(results));
       }
       return;
+    case 'SET_ARTIST':
+      return state.set('artist_name', action.name);
     case 'SHOW_ALBUMS':
       let albumSet = new Set(state.get('show_albums'));
 
@@ -25,6 +29,9 @@ export const queryReducer = (state = immutableState, action) => {
       }
 
       return state.set('show_albums', albumSet);
+    case 'SET_PAGINATION_LOCATON':
+      let newLoc = state.get('pagination_location') + action.integer;
+      return state.set('pagination_location', newLoc);
     default:
       return state;
   }
